472d3c7e725b2a2b7df7efda2d1ebbd1819f65c6
GAMERWES01 <148088336+GAMERWES01@users.noreply.github.com>
2024-07-02 22:33:19 +0200

    resize gemaakt met demo met lelijke figuren
    kijk titel

--- Frame.java
+++ Frame.java
@@ -3,17 +3,17 @@ import java.awt.*;
 
 public class Frame extends JFrame {
 
+    Panel panel = new Panel();
+
     public Frame(){
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Storm-D&D");
 
         setLayout(new BorderLayout());
-        Panel panel = new Panel();
         add(panel, BorderLayout.CENTER);
         pack();
         setLocationRelativeTo(null);
-
-
     }
+
 
 }
--- Panel.java
+++ Panel.java
@@ -5,8 +5,7 @@ public class Panel extends JPanel {
 
     public Panel() {
         setBackground(new Color(17, 38, 58));  // Achtergrond kleur
-        setPreferredSize(new Dimension(100,100));
-
+        setPreferredSize(new Dimension(200,200));
     }
 
     @Override
@@ -14,6 +13,15 @@ public class Panel extends JPanel {
         super.paintComponent(g);
 
         g.setColor(Color.black);
-        g.fillRect(0,0,1000,100);
+        g.fillRect(0,0,getWidth(),getHeight());
+        g.setColor(Color.yellow);
+        g.fillOval(0,0,getWidth(),getHeight());
+        g.setColor(Color.green);
+        g.fillRect(0,0,getWidth()/2, getHeight()/2);
+
+
     }
+
+
+
 }
