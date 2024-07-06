07bf785b3a1650fc873db13359ec41b205aaa34c
GAMERWES01 <148088336+GAMERWES01@users.noreply.github.com>
2024-07-07 00:34:37 +0200

    full screen modus
    full screen modus toegevoegt
    heeft nog een hele kleine bug die ik zelf nie kan fixen namelijk als je full screen bent en je druk op je 2 screm en opent hem darna weer gaat hij raar doen weet niet warom dat komt

--- Frame.java
+++ Frame.java
@@ -1,19 +1,48 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Frame extends JFrame {
+public class Frame extends JFrame implements KeyListener {
 
-    Panel panel = new Panel();
+
+    GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
 
     public Frame(){
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Storm-D&D");
-
         setLayout(new BorderLayout());
+
+        Panel panel = new Panel();
         add(panel, BorderLayout.CENTER);
         pack();
         setLocationRelativeTo(null);
+
+        addKeyListener(this);
     }
 
 
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyPressed(KeyEvent e) {}
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_F11) {
+            if (gd.getFullScreenWindow() == null){
+                if (gd.isFullScreenSupported()) {
+                    gd.setFullScreenWindow(this);
+
+                }
+                else {
+                    System.err.println("Full screen not supported");
+                }
+            } else {
+                gd.setFullScreenWindow(null);
+            }
+        }
+    }
 }
--- Panel.java
+++ Panel.java
@@ -1,16 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Panel extends JPanel {
 
     public Panel() {
         setBackground(new Color(17, 38, 58));  // Achtergrond kleur
-        setPreferredSize(new Dimension(200,200));
+        setPreferredSize(new Dimension(192,100));
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+
+        double x = (double) getWidth() /192;
+        double y = (double) getHeight() /100;
 
         g.setColor(Color.black);
         g.fillRect(0,0,getWidth(),getHeight());
@@ -18,6 +23,7 @@ public class Panel extends JPanel {
         g.fillOval(0,0,getWidth(),getHeight());
         g.setColor(Color.green);
         g.fillRect(0,0,getWidth()/2, getHeight()/2);
+        g.fillRect((int) (96*x), (int) (50*y), (int) (96*x), (int) (50*y));
 
 
     }
